59c473b6377248a7a4b5310b6b66a5876f14863a
Vebjørn Johansen Rognli <vebjornjr@gmail.com>
2023-10-24 22:20:21 +0200

    CMake project config which suports non-ROS build
    Signed-off-by: Vebjørn Johansen Rognli <vebjornjr@gmail.com>

---
+++ steering_functionsConfig.cmake
@@ -0,0 +1,7 @@
+include(CMakeFindDependencyMacro)
+
+# Same syntax as find_package
+find_dependency(Eigen3 REQUIRED)
+
+# Add the targets file
+include("${CMAKE_CURRENT_LIST_DIR}/steering_functionsTargets.cmake")
